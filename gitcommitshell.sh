8c5bfbea2a9060cd01abaeb4637470d545f67a8e
windhaunting <fubaowu@gmail.com>
2021-07-06 20:09:03 -0400

    add_file

--- gitcommitshell.sh
+++ gitcommitshell.sh
@@ -1,5 +1,6 @@
 
-cd /home/fubao/workDir/Miscs/personalWebsite/githubHomepage/windhaunting.github.io/
+#cd /home/fubao/workDir/Miscs/personalWebsite/githubHomepage/windhaunting.github.io/
+cd /media/fubao/TOSHIBAEXT/backup_laptopt440p/Miscs/personalWebsite/personalWebsite/githubHomepage/windhaunting.github.io
 #git commit $1
 git commit -a -m  $1
 git push origin master
